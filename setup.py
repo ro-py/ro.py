740b58020e566f8ef05912a8b94a00c20a15c1cf
jmkdev <jmk@jmksite.dev>
2020-12-05 14:31:53 -0500

    New Gitignore + setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ro-py",  # Replace with your own username
+    version="0.0.1",
+    author="jmkdev",
+    author_email="jmk@jmksite.dev",
+    description="ro.py is a Python wrapper for the Roblox web API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jmk-developer/ro.py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
