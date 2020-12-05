63802a0368e0e1c9416c00a9208ad69edb18821c
jmkdev <jmk@jmksite.dev>
2020-12-05 12:08:36 -0500

    Groups!

---
+++ ro_py/groups.py
@@ -0,0 +1,18 @@
+from ro_py import User
+import requests
+
+endpoint = "https://groups.roblox.com/"
+
+
+class Group:
+    def __init__(self, group_id):
+        self.id = group_id
+        group_info_req = requests.get(endpoint + f"v1/groups/{self.id}")
+        group_info = group_info_req.json()
+        self.name = group_info["name"]
+        self.description = group_info["description"]
+        self.owner = User(group_info["owner"]["userId"])
+        self.member_count = group_info["memberCount"]
+        self.is_builders_club_only = group_info["isBuildersClubOnly"]
+        self.public_entry_allowed = group_info["public_entry_allowed"]
+        self.is_locked = group_info["isLocked"]
