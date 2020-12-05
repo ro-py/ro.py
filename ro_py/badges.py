ebb4ed07c6cb5ada627a1b964a8ea9ea8e25b004
jmkdev <jmk@jmksite.dev>
2020-12-04 20:49:53 -0500

    Users and badges!

---
+++ ro_py/badges.py
@@ -0,0 +1,29 @@
+from roa import read_only_attributes
+import requests
+
+endpoint = "http://badges.roblox.com/"
+
+
+class BadgeStatistics:
+    def __init__(self, past_date_awarded_count, awarded_count, win_rate_percentage):
+        self.past_date_awarded_count = past_date_awarded_count
+        self.awarded_count = awarded_count
+        self.win_rate_percentage = win_rate_percentage
+
+
+class Badge:
+    def __init__(self, badge_id):
+        self.id = badge_id
+        badge_info_req = requests.get(endpoint + f"v1/badges/{badge_id}")
+        badge_info = badge_info_req.json()
+        self.name = badge_info["name"]
+        self.description = badge_info["description"]
+        self.display_name = badge_info["displayName"]
+        self.display_description = badge_info["displayDescription"]
+        self.enabled = badge_info["enabled"]
+        statistics_info = badge_info["statistics"]
+        self.statistics = BadgeStatistics(
+            statistics_info["pastDayAwardedCount"],
+            statistics_info["awardedCount"],
+            statistics_info["winRatePercentage"]
+        )
--- ro_py/users.py
+++ ro_py/users.py
@@ -1,11 +1,13 @@
+from roa import read_only_attributes
 import requests
 
 endpoint = "http://users.roblox.com/"
 
 
+@read_only_attributes("user_id", "description", "is_banned", "name", "display_name")
 class User:
     def __init__(self, user_id):
-        self.user_id = user_id
+        self.id = user_id
         user_info_req = requests.get(endpoint + f"v1/users/{user_id}")
         user_info = user_info_req.json()
         self.description = user_info["description"]
@@ -15,3 +17,4 @@ class User:
 
     def get_status(self):
         status_req = requests.get(endpoint + f"v1/users/{self.user_id}/status")
+        return status_req.json()["status"]
