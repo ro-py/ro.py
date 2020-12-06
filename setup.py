1761821aa4846f0791ce29d09852de58bd9c97f1
jmkdev <jmk@jmksite.dev>
2020-12-05 21:42:39 -0500

    Updated version identifier

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ro-py",
-    version="0.0.3",
+    version="0.0.4",
     author="jmkdev",
     author_email="jmk@jmksite.dev",
     description="ro.py is a Python wrapper for the Roblox web API.",
