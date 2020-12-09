8aa456397660eb57230edb0e02f9010449a95303
jmkdev <jmk@jmksite.dev>
2020-12-09 13:15:49 -0500

    Create chat.py

---
+++ ro_py/chat.py
@@ -0,0 +1,12 @@
+"""
+
+ro.py > chat.py
+
+This file houses functions and classes that pertain to chatting and messaging.
+
+"""
+
+
+class ChatWrapper:
+    def __init__(self, requests):
+        self.requests = requests
