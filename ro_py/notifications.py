18a4f43b0d5fd4b27748c6a358a2c2b4bf2e9a58
mfd <minigunneryt@gmail.com>
2020-12-11 15:09:58 +0000

    Things are broken

--- ro_py/notifications.py
+++ ro_py/notifications.py
@@ -1,44 +1,49 @@
-from signalrcore.hub_connection_builder import HubConnectionBuilder
+import requests
+from urllib.parse import quote
+import signalrcore.hub_connection_builder
+from time import sleep
 
 
 class NotificationReceiver:
-    def __init__(self, requests, on_open, on_close, on_error):
-        self.requests = requests
-
-        self.on_open = on_open
-        self.on_close = on_close
-        self.on_error = on_error
-
-        self.roblosecurity = self.requests.cookies[".ROBLOSECURITY"]
-        self.negotiate_request = self.requests.get(
-            url="https://realtime.roblox.com/notifications/negotiate"
-                "?clientProtocol=1.5"
-                "&connectionData=%5B%7B%22name%22%3A%22usernotificationhub%22%7D%5D",
+    def __init__(self, rs):
+        a = requests.get(
+            url="https://realtime.roblox.com/notifications/negotiate?clientProtocol=1.5&connectionData=%5B%7B%22name"
+                "%22%3A%22usernotificationhub%22%7D%5D",
             cookies={
-                ".ROBLOSECURITY": self.roblosecurity
+                ".ROBLOSECURITY": rs
             }
         )
-        self.wss_url = f"wss://realtime.roblox.com/notifications?transport=websockets" \
-                       f"&connectionToken={self.negotiate_request.json()['ConnectionToken']}" \
-                       f"&clientProtocol=1.5&connectionData=%5B%7B%22name%22%3A%22usernotificationhub%22%7D%5D"
-        self.connection = HubConnectionBuilder()
-        self.connection.with_url(
-            self.wss_url,
+
+        print(a.text)
+
+        final_url = f"wss://realtime.roblox.com/notifications?transport=websockets" \
+                    f"&connectionToken={quote(a.json()['ConnectionToken'])}" \
+                    f"&clientProtocol=1.5&connectionData=%5B%7B%22name%22%3A%22usernotificationhub%22%7D%5D"
+
+        print(final_url)
+
+        hub_connection = signalrcore.hub_connection_builder.HubConnectionBuilder()
+
+        hub_connection.with_url(
+            final_url,
             options={
                 "headers": {
-                    "Cookie": f".ROBLOSECURITY={self.roblosecurity};"
+                    "Cookie": f".ROBLOSECURITY={rs}"
                 }
             }
         )
 
-        self.connection.with_automatic_reconnect({
+        hub_connection.with_automatic_reconnect({
             "type": "raw",
             "keep_alive_interval": 10,
             "reconnect_interval": 5,
             "max_attempts": 5
         }).build()
 
-        self.connection.on_open(self.on_open)
-        self.connection.on_close(self.on_close)
-        self.connection.on_error(self.on_error)
-        self.connection.start()
+        hub_connection.on_open(lambda d: print(d))
+        hub_connection.on_close(lambda d: print(d))
+        hub_connection.on("usernotificationhub", lambda: print("got thing"))
+        hub_connection.start()
+
+        while True:
+            sleep(1 / 60)
