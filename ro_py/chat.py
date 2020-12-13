916b780cbaf3ffc3e3679f8e6cc78d09cca72fff
jmkdev <jmk@jmksite.dev>
2020-12-13 13:06:07 -0500

    ConversationTyping + endpoint

--- ro_py/chat.py
+++ ro_py/chat.py
@@ -8,15 +8,42 @@ This file houses functions and classes that pertain to chatting and messaging.
 
 from ro_py.utilities.errors import ChatError
 
+endpoint = "https://chat.roblox.com/"
+
+
+class ConversationTyping:
+    def __init__(self, requests, conversation_id):
+        self.requests = requests
+        self.id = conversation_id
+
+    def __enter__(self):
+        self.requests.post(
+            url=endpoint + "v2/update-user-typing-status",
+            data={
+                "conversationId": self.id,
+                "isTyping": "true"
+            }
+        )
+
+    def __exit__(self):
+        self.requests.post(
+            url=endpoint + "v2/update-user-typing-status",
+            data={
+                "conversationId": self.id,
+                "isTyping": "false"
+            }
+        )
+
 
 class Conversation:
     def __init__(self, requests, conversation_id):
         self.requests = requests
         self.id = conversation_id
+        self.typing = ConversationTyping(self.requests, conversation_id)
 
     def send_message(self, message):
         send_message_req = self.requests.post(
-            url="https://chat.roblox.com/v2/send-message",
+            url=endpoint + "v2/send-message",
             data={
                 "message": message,
                 "conversationId": self.id
