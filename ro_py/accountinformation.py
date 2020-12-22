051bdd847fb90ba861bfaa701fbb8bf47a31487c
jmkdev <jmk@jmksite.dev>
2020-12-22 11:46:09 -0500

    trying to revert some things, again

--- ro_py/accountinformation.py
+++ ro_py/accountinformation.py
@@ -17,13 +17,12 @@ class AccountInformationMetadata:
     Represents account information metadata.
     """
     def __init__(self, metadata_raw):
-        self.__dict__["is_allowed_notifications_endpoint_disabled"] = \
-            metadata_raw["isAllowedNotificationsEndpointDisabled"]
-        self.__dict__["is_account_settings_policy_enabled"] = metadata_raw["isAccountSettingsPolicyEnabled"]
-        self.__dict__["is_phone_number_enabled"] = metadata_raw["isPhoneNumberEnabled"]
-        self.__dict__["max_user_description_length"] = metadata_raw["MaxUserDescriptionLength"]
-        self.__dict__["is_user_description_enabled"] = metadata_raw["isUserDescriptionEnabled"]
-        self.__dict__["is_user_block_endpoints_updated"] = metadata_raw["isUserBlockEndpointsUpdated"]
+        self.is_allowed_notifications_endpoint_disabled = metadata_raw["isAllowedNotificationsEndpointDisabled"]
+        self.is_account_settings_policy_enabled = metadata_raw["isAccountSettingsPolicyEnabled"]
+        self.is_phone_number_enabled = metadata_raw["isPhoneNumberEnabled"]
+        self.max_user_description_length = metadata_raw["MaxUserDescriptionLength"]
+        self.is_user_description_enabled = metadata_raw["isUserDescriptionEnabled"]
+        self.is_user_block_endpoints_updated = metadata_raw["isUserBlockEndpointsUpdated"]
 
 
 class PromotionChannels:
@@ -31,31 +30,11 @@ class PromotionChannels:
     Represents account information promotion channels.
     """
     def __init__(self, promotion_raw):
-        self.__dict__["promotion_channels_visibility_privacy"] = promotion_raw["promotionChannelsVisibilityPrivacy"]
-        self.__dict__["facebook"] = promotion_raw["facebook"]
-        self.__dict__["twitter"] = promotion_raw["twitter"]
-        self.__dict__["youtube"] = promotion_raw["youtube"]
-        self.__dict__["twitch"] = promotion_raw["twitch"]
-
-    @property
-    def promotion_channels_visibility_privacy(self):
-        return self.__dict__["promotion_channels_visibility_privacy"]
-
-    @property
-    def facebook(self):
-        return self.__dict__["facebook"]
-
-    @property
-    def twitter(self):
-        return self.__dict__["twitter"]
-
-    @property
-    def youtube(self):
-        return self.__dict__["youtube"]
-
-    @property
-    def twitch(self):
-        return self.__dict__["twitch"]
+        self.promotion_channels_visibility_privacy = promotion_raw["promotionChannelsVisibilityPrivacy"]
+        self.facebook = promotion_raw["facebook"]
+        self.twitter = promotion_raw["twitter"]
+        self.youtube = promotion_raw["youtube"]
+        self.twitch = promotion_raw["twitch"]
 
 
 class AccountInformation:
@@ -66,6 +45,7 @@ class AccountInformation:
     def __init__(self, requests):
 <<<<<<< HEAD
         self.__dict__["requests"] = requests
+<<<<<<< HEAD
         self.__dict__["account_information_metadata"] = None
         self.__dict__["promotion_channels"] = None
 =======
@@ -73,6 +53,10 @@ class AccountInformation:
         self.account_information_metadata = None
         self.promotion_channels = None
 >>>>>>> parent of 5a7703f... Read only requests
+=======
+        self.account_information_metadata = None
+        self.promotion_channels = None
+>>>>>>> parent of f574140... AccountInformation and AccountSettings are read-only
         self.update()
 
     def update(self):
@@ -80,6 +64,7 @@ class AccountInformation:
         Updates the account information.
         :return: Nothing
         """
+<<<<<<< HEAD
 <<<<<<< HEAD
         account_information_req = self.__dict__["requests"].get(
             url="https://accountinformation.roblox.com/v1/metadata"
@@ -104,6 +89,12 @@ class AccountInformation:
         promotion_channels_req = self.requests.get("https://accountinformation.roblox.com/v1/promotion-channels")
         self.promotion_channels = PromotionChannels(promotion_channels_req.json())
 >>>>>>> parent of 5a7703f... Read only requests
+=======
+        account_information_req = self.__dict__["requests"].get("https://accountinformation.roblox.com/v1/metadata")
+        self.account_information_metadata = AccountInformationMetadata(account_information_req.json())
+        promotion_channels_req = self.__dict__["requests"].get("https://accountinformation.roblox.com/v1/promotion-channels")
+        self.promotion_channels = PromotionChannels(promotion_channels_req.json())
+>>>>>>> parent of f574140... AccountInformation and AccountSettings are read-only
 
     def get_gender(self):
         """
