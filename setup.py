f8db8254b8cbe7d4f34489ea2c8d27c065a7a869
jmkdev <jmk@jmksite.dev>
2020-12-21 19:25:12 -0500

    Incorrect license

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GPL License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
