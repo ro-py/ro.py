65345d87e044b5a778b10b3cca0d7c7237b09269
jmkdev <jmk@jmksite.dev>
2020-12-26 15:50:24 -0500

    Updated main doc

--- ro_py/__init__.py
+++ ro_py/__init__.py
@@ -1,13 +1,11 @@
-r"""
-
-  _ __ ___   _ __  _   _
- | '__/ _ \ | '_ \| | | |
- | | | (_) || |_) | |_| |
- |_|  \___(_) .__/ \__, |
-            | |     __/ |
-            |_|    |___/
+"""
 
 ro.py
 by jmkdev
+
+Welcome to ro.py!
+ro.py is a powerful wrapper for the Roblox web API.
+It can be used to create (almost) anything from chat bots to group management systems.
+ro.py is still fairly new and may have bugs. If you have any issues, you can contact me at
 
 """
