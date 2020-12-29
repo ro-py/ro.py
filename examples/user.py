31b3013bb11049017c786359e0a3c1929d71cc35
jmkdev <jmk@jmksite.dev>
2020-12-29 14:56:54 -0500

    Fixed user example

--- examples/user.py
+++ examples/user.py
@@ -1,14 +1,26 @@
 from ro_py.client import Client
+import asyncio
 
 client = Client()
 
 user_id = 576059883
 
-print(f"Loading user {user_id}...")
-user = client.get_user(user_id)
-print("Loaded user.")
 
-print(f"Username: {user.name}")
-print(f"Display Name: {user.display_name}")
-print(f"Description: {user.description}")
-print(f"Status: {user.get_status() or 'None.'}")
+async def grab_info():
+    print(f"Loading user {user_id}...")
+    user = await client.get_user(user_id)
+    print("Loaded user.")
+
+    print(f"Username: {user.name}")
+    print(f"Display Name: {user.display_name}")
+    print(f"Description: {user.description}")
+    print(f"Status: {await user.get_status() or 'None.'}")
+
+
+def main():
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(grab_info())
+
+
+if __name__ == '__main__':
+    main()
