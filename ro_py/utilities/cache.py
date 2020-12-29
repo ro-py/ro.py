4e31e7097e468650d355317265ede72741aac0d8
jmkdev <jmk@jmksite.dev>
2020-12-29 17:15:31 -0500

    Update cache.py

--- ro_py/utilities/cache.py
+++ ro_py/utilities/cache.py
@@ -1,3 +1,14 @@
+import enum
+
+
+class CacheType(enum.Enum):
+    Users = "users"
+    Groups = "groups"
+    Games = "games"
+    Assets = "assets"
+    Badges = "badges"
+
+
 class Cache:
     def __init__(self):
         self.cache = {
@@ -6,4 +17,13 @@ class Cache:
             "games": {},
             "assets": {},
             "badges": {}
-    }
+        }
+
+    def get(self, cache_type: CacheType, item_id: str):
+        if item_id in self.cache[cache_type.value]:
+            return self.cache[cache_type.value][item_id]
+        else:
+            return False
+
+    def set(self, cache_type: CacheType, item_id: str, item_obj):
+        self.cache[cache_type.value][item_id] = item_obj
