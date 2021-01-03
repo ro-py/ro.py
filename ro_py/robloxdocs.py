c3fd035f700f0782c0407dc39fcf90d8d9541f86
jmkdev <jmk@jmksite.dev>
2021-01-03 16:23:38 -0500

    working on RobloxDocs

--- ro_py/robloxdocs.py
+++ ro_py/robloxdocs.py
@@ -10,6 +10,20 @@ from lxml import html
 from io import StringIO
 
 
+class EndpointDocsDataInfo:
+    def __init__(self, data):
+        self.version = data["version"]
+        self.title = data["title"]
+
+
+class EndpointDocsData:
+    def __init__(self, data):
+        self.swagger_version = data["swagger"]
+        self.info = EndpointDocsDataInfo(data["info"])
+        self.host = data["host"]
+        self.schemes = data["schemes"]
+
+
 class EndpointDocs:
     def __init__(self, requests, docs_url):
         self.requests = requests
@@ -17,3 +31,14 @@ class EndpointDocs:
 
     async def get_versions(self):
         docs_req = self.requests.get(self.url + "/docs")
+        root = html.parse(StringIO(docs_req.text)).getroot()
+        try:
+            vs_element = root.get_element_by_id("version-selector")
+            return vs_element.value_options
+        except KeyError:
+            return ["v1"]
+
+    async def get_data_for_version(self, version):
+        data_req = self.requests.get(self.url + "/docs/json/" + version)
+        version_data = data_req.json()
+        return EndpointDocsData(version_data)
