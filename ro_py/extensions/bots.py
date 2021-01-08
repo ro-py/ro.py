dbdeec79c543e9f2032508e173cf6208b90e59d1
jmkdev <jmk@jmksite.dev>
2021-01-08 10:31:22 -0500

    Working on bots object

--- ro_py/extensions/bots.py
+++ ro_py/extensions/bots.py
@@ -3,3 +3,25 @@
 This extension houses functions that allow generation of Bot objects, which interpret commands.
 
 """
+
+
+from ro_py.client import Client
+
+
+class Bot(Client):
+    def __init__(self):
+        super().__init__()
+
+
+class Command:
+    def __init__(self):
+        pass
+
+
+def command(function):
+    def decorator(func):
+        if isinstance(func, Command):
+            raise TypeError('Callback is already a command.')
+        return Command(func, name=name, **attrs)
+
+    return decorator
