7036356dccf565b25cc6f54994c0045b40c038ec
jmkdev <jmk@jmksite.dev>
2021-01-14 22:17:54 -0500

    New init docs with HTML

--- ro_py/__init__.py
+++ ro_py/__init__.py
@@ -1,13 +1,18 @@
 """
 
-ro.py
-by jmkdev
-
-Welcome to ro.py!
-ro.py is a powerful wrapper for the Roblox web API.
-It can be used to create (almost) anything from chat bots to group management systems.
-ro.py is still fairly new and may have bugs. If you have any issues, you can contact me at https://jmksite.dev/
-You can view the source code at https://github.com/jmk-developer/ro.py/
-You can also view the documentation at https://ro.py.jmksite.dev/.
+<h1 align="center">
+    <img src="https://raw.githubusercontent.com/rbx-libdev/ro.py/main/resources/header.png" alt="ro.py" width="400" />
+    <br>
+</h1>
+<h4 align="center">ro.py is a powerful Python 3 wrapper for the Roblox Web API.</h4>
+<p align="center">
+  <a href="https://github.com/rbx-libdev/ro.py#information">Information</a> |
+  <a href="https://github.com/rbx-libdev/ro.py#requirements">Requirements</a> |
+  <a href="https://github.com/rbx-libdev/ro.py#disclaimer">Disclaimer</a> |
+  <a href="https://github.com/rbx-libdev/ro.py#documentation">Documentation</a> |
+  <a href="https://github.com/rbx-libdev/ro.py/tree/main/examples">Examples</a> |
+  <a href="https://github.com/rbx-libdev/ro.py#credits">Credits</a> |
+  <a href="https://github.com/rbx-libdev/ro.py/blob/main/LICENSE">License</a>
+</p>
 
 """
