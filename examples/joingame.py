528ae43e5709becca623d64fea559cd801fad42a
jmkdev <jmk@jmksite.dev>
2021-01-15 08:37:31 -0500

    Better joingame example

--- examples/joingame.py
+++ examples/joingame.py
@@ -10,17 +10,16 @@ This example logs in with a GUI and then joins Sword Fights on the Heights IV.
 import asyncio
 from ro_py.client import Client
 from ro_py.extensions.prompt import authenticate_prompt
-from ro_py.games import Place  # hacky, will fix soon
 
 client = Client()
 
 
 async def main():
+    game = await client.get_game_by_place_id(47324)  # ⚔️ Sword Fights on the Heights IV
     auth_prompt = await authenticate_prompt(client)
     print(f"Authenticated: {auth_prompt}")
     if auth_prompt:
-        place = Place(client.requests, 47324)  # Sword Fights On The Heights IV
-        await place.join()
+        await game.root_place.join()
     else:
         print("Failed to authenticate.")
 
