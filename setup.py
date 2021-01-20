e25f05d55dfb81a1fe2a6e2d99340a2fcfa14168
jmkdev <jmk@jmksite.dev>
2021-01-20 14:06:55 -0500

    Modified setup behavior

--- setup.py
+++ setup.py
@@ -1,30 +1,7 @@
 import setuptools
+import setup_info
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="ro-py",
-    version="1.1.0",
-    author="jmkdev and iranathan",
-    author_email="jmk@jmksite.dev",
-    description="ro.py is a Python wrapper for the Roblox web API.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/rbx-libdev/ro.py",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-    install_requires=[
-        "httpx",
-        "iso8601",
-        "signalrcore",
-        "pytweening",
-        "wxPython",
-        "wxasync"
-    ]
-)
+setuptools.setup(**setup_info.setup_info)
---
+++ setup_info.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup_info = {
+    "name": "ro-py",
+    "version": "1.1.0",
+    "author": "jmkdev and iranathan",
+    "author_email": "jmk@jmksite.dev",
+    "description": "ro.py is a Python wrapper for the Roblox web API.",
+    "long_description": long_description,
+    "long_description_content_type": "text/markdown",
+    "url": "https://github.com/rbx-libdev/ro.py",
+    "packages": setuptools.find_packages(),
+    "classifiers": [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    "python_requires": '>=3.6',
+    "install_requires": [
+        "httpx",
+        "iso8601",
+        "signalrcore",
+        "pytweening",
+        "wxPython",
+        "wxasync"
+    ]
+}
