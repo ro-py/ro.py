ac6a2392047c9be3d6bea65d9f8acaaa3a09629e
jmkdev <jmk@jmksite.dev>
2021-01-31 15:48:57 -0500

    Docs updated + badges in docs

--- ro_py/__init__.py
+++ ro_py/__init__.py
@@ -4,15 +4,26 @@
     <img src="https://raw.githubusercontent.com/rbx-libdev/ro.py/main/resources/header.png" alt="ro.py" width="400" />
     <br>
 </h1>
-<h4 align="center">ro.py is a powerful Python 3 wrapper for the Roblox Web API.</h4>
+<h4 align="center">ro.py is a powerful Python 3 wrapper for the Roblox Web API by <a href="https://github.com/jmkd3v">@jmkd3v</a> and <a href="https://github.com/iranathan">@iranathan</a>.</h4>
+
 <p align="center">
-  <a href="https://github.com/rbx-libdev/ro.py#information">Information</a> |
-  <a href="https://github.com/rbx-libdev/ro.py#requirements">Requirements</a> |
-  <a href="https://github.com/rbx-libdev/ro.py#disclaimer">Disclaimer</a> |
-  <a href="https://github.com/rbx-libdev/ro.py#documentation">Documentation</a> |
-  <a href="https://github.com/rbx-libdev/ro.py/tree/main/examples">Examples</a> |
-  <a href="https://github.com/rbx-libdev/ro.py#credits">Credits</a> |
-  <a href="https://github.com/rbx-libdev/ro.py/blob/main/LICENSE">License</a>
+    <a href="https://j-mk.ml/ro.py"><img src="https://img.shields.io/discord/761603917490159676?style=flat-square&logo=discord" alt="ro.py Discord"/></a>
+    <a href="https://pypi.org/project/ro-py/"><img src="https://img.shields.io/pypi/v/ro-py?style=flat-square" alt="ro.py PyPI"/></a>
+    <a href="https://pypi.org/project/ro-py/"><img src="https://img.shields.io/pypi/dm/ro-py?style=flat-square" alt="ro.py PyPI Downloads"/></a>
+    <a href="https://pypi.org/project/ro-py/"><img src="https://img.shields.io/pypi/l/ro-py?style=flat-square" alt="ro.py PyPI License"/></a>
+    <a href="https://github.com/rbx-libdev/ro.py"><img src="https://img.shields.io/github/commit-activity/w/rbx-libdev/ro.py?style=flat-square" alt="ro.py GitHub Commit Activity"/></a>
+    <a href="https://github.com/rbx-libdev/ro.py"><img src="https://img.shields.io/github/last-commit/rbx-libdev/ro.py?style=flat-square" alt="ro.py GitHub Last Commit"/></a>
+</p>
+
+<p align="center">
+    <a href="https://github.com/rbx-libdev/ro.py#information">Information</a> |
+    <a href="http://j-mk.ml/ro.py">Discord</a> |
+    <a href="https://github.com/rbx-libdev/ro.py#requirements">Requirements</a> |
+    <a href="https://github.com/rbx-libdev/ro.py#disclaimer">Disclaimer</a> |
+    <a href="https://github.com/rbx-libdev/ro.py#documentation">Documentation</a> |
+    <a href="https://github.com/rbx-libdev/ro.py/tree/main/examples">Examples</a> |
+    <a href="https://github.com/rbx-libdev/ro.py#credits">Credits</a> |
+    <a href="https://github.com/rbx-libdev/ro.py/blob/main/LICENSE">License</a>
 </p>
 
 """
