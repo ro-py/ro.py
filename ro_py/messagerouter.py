28887996dee925ac2df044fc828b401de46ec9fd
ROBLOX Team Foundation <github@roblox.com>
2021-03-08 13:49:58 +0000

    Stage the messagerouter api

---
+++ ro_py/messagerouter.py
@@ -0,0 +1,13 @@
+"""
+
+This file houses functions and classes that are part of the Message Router API
+
+"""
+
+endpoint = "https://messagerouter.api.roblox.com/"
+endpoint_dev = "https://messagerouter.api.sitetest4.robloxlabs.com"
+
+
+class MessageRouter:
+    def __init__(self):
+        return
