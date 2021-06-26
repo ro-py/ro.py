6a56df24a0796d144d35525403a03aab96ca494c
Boegie19 <34578426+Boegie19@users.noreply.github.com>
2021-06-26 12:53:32 +0200

    Added class variable action and used it is Desction.from_action

--- roblox/auditlogs.py
+++ roblox/auditlogs.py
@@ -11,6 +11,50 @@ import datetime
 import roblox.utilities.clientshardobject
 
 
+class Actions(Enum):
+    delete_post = "deletePost"
+    remove_member = "removeMember"
+    accept_join_request = "Accept Join Request"
+    decline_join_request = "declineJoinRequest"
+    post_shout = "postShout"
+    change_rank = "changeRank"
+    buy_ad = "buyAd"
+    send_ally_request = "sendAllyRequest"
+    create_enemy = "createEnemy"
+    accept_ally_request = "acceptAllyRequest"
+    decline_ally_request = "declineAllyRequest"
+    delete_ally = "deleteAlly"
+    add_group_place = "addGroupPlace"
+    delete_group_place = "deleteGroupPlace"
+    create_items = "createItems"
+    configure_items = "configureItems"
+    spend_group_funds = "spendGroupFunds"
+    change_owner = "changeOwner"
+    delete = "delete"
+    adjust_currency_amounts = "adjustCurrencyAmounts"
+    abandon = "abandon"
+    claim = "claim"
+    rename = "rename"
+    change_description = "changeDescription"
+    create_group_asset = "createGroupAsset"
+    upload_group_asset = "uploadGroupAsset"
+    configure_group_asset = "configureGroupAsset"
+    revert_group_asset = "revertGroupAsset"
+    create_group_developer_product = "createGroupDeveloperProduct"
+    configure_group_game = "configureGroupGame"
+    lock = "lock"
+    unlock = "unlock"
+    create_game_pass = "createGamePass"
+    create_badge = "createBadge"
+    configure_badge = "configureBadge"
+    save_place = "savePlace"
+    publish_place = "publishPlace"
+    invite_to_clan = "inviteToClan"
+    kick_from_clan = "kickFromClan"
+    cancel_clan_invite = "cancelClanInvite"
+    buy_clan = "buyClan"
+
+
 class Action:
     def __init__(self, cso: roblox.utilities.clientshardobject.ClientSharedObject,
                  group: roblox.bases.basegroup.BaseGroup, raw_data: dict):
@@ -27,119 +71,37 @@ class Action:
 
 
 class Description:
+    action = None
 
-    def __init__(self, type, description):
-        self.action: str = type
+    def __init__(self, description):
+        pass
 
     @classmethod
     def from_action(cls, action, description) -> Description:
         for c in cls.__subclasses__():
-            if ActionsToClasses[action]:
+            if c.action == action:
                 break
         else:
             raise ValueError("Unknown type: {!r}".format(action))
-        return c(action, description)
+        return c(description)
 
 
 class DeletePost(Description):
+    action = Actions.delete_post
 
     def __init__(self, action, description):
-        super().__init__(action, description)
+        super().__init__(description)
 
 
 class RemoveMember(Description):
+    action = Actions.remove_member
+
     def __init__(self, action, description):
-        super().__init__(action, description)
+        super().__init__(description)
 
 
 class AcceptJoinRequest(Description):
+    action = Actions.accept_join_request
+
     def __init__(self, action, description):
-        super().__init__(action, description)
-
-
-class ActionsToClasses(Enum):
-    deletePost = DeletePost
-    removeMember = RemoveMember
-    accept_join_request = AcceptJoinRequest
-    decline_join_request = "declineJoinRequest"
-    post_shout = "postShout"
-    change_rank = "changeRank"
-    buy_ad = "buyAd"
-    send_ally_request = "sendAllyRequest"
-    create_enemy = "createEnemy"
-    accept_ally_request = "acceptAllyRequest"
-    decline_ally_request = "declineAllyRequest"
-    delete_ally = "deleteAlly"
-    add_group_place = "addGroupPlace"
-    delete_group_place = "deleteGroupPlace"
-    create_items = "createItems"
-    configure_items = "configureItems"
-    spend_group_funds = "spendGroupFunds"
-    change_owner = "changeOwner"
-    delete = "delete"
-    adjust_currency_amounts = "adjustCurrencyAmounts"
-    abandon = "abandon"
-    claim = "claim"
-    rename = "rename"
-    change_description = "changeDescription"
-    create_group_asset = "createGroupAsset"
-    upload_group_asset = "uploadGroupAsset"
-    configure_group_asset = "configureGroupAsset"
-    revert_group_asset = "revertGroupAsset"
-    create_group_developer_product = "createGroupDeveloperProduct"
-    configure_group_game = "configureGroupGame"
-    lock = "lock"
-    unlock = "unlock"
-    create_game_pass = "createGamePass"
-    create_badge = "createBadge"
-    configure_badge = "configureBadge"
-    save_place = "savePlace"
-    publish_place = "publishPlace"
-    invite_to_clan = "inviteToClan"
-    kick_from_clan = "kickFromClan"
-    cancel_clan_invite = "cancelClanInvite"
-    buy_clan = "buyClan"
-
-
-class Actions(Enum):
-    delete_post = "deletePost"
-    remove_member = "removeMember"
-    accept_join_request = "acceptJoinRequest"
-    decline_join_request = "declineJoinRequest"
-    post_shout = "postShout"
-    change_rank = "changeRank"
-    buy_ad = "buyAd"
-    send_ally_request = "sendAllyRequest"
-    create_enemy = "createEnemy"
-    accept_ally_request = "acceptAllyRequest"
-    decline_ally_request = "declineAllyRequest"
-    delete_ally = "deleteAlly"
-    add_group_place = "addGroupPlace"
-    delete_group_place = "deleteGroupPlace"
-    create_items = "createItems"
-    configure_items = "configureItems"
-    spend_group_funds = "spendGroupFunds"
-    change_owner = "changeOwner"
-    delete = "delete"
-    adjust_currency_amounts = "adjustCurrencyAmounts"
-    abandon = "abandon"
-    claim = "claim"
-    rename = "rename"
-    change_description = "changeDescription"
-    create_group_asset = "createGroupAsset"
-    upload_group_asset = "uploadGroupAsset"
-    configure_group_asset = "configureGroupAsset"
-    revert_group_asset = "revertGroupAsset"
-    create_group_developer_product = "createGroupDeveloperProduct"
-    configure_group_game = "configureGroupGame"
-    lock = "lock"
-    unlock = "unlock"
-    create_game_pass = "createGamePass"
-    create_badge = "createBadge"
-    configure_badge = "configureBadge"
-    save_place = "savePlace"
-    publish_place = "publishPlace"
-    invite_to_clan = "inviteToClan"
-    kick_from_clan = "kickFromClan"
-    cancel_clan_invite = "cancelClanInvite"
-    buy_clan = "buyClan"
+        super().__init__(description)
