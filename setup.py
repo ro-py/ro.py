7f40e59feddacb7df54ec54f5199d4ba2c6519c5
jmkd3v <jmk@jmksite.dev>
2021-08-19 14:58:20 -0400

    Improved description stuff

--- setup.py
+++ setup.py
@@ -1,7 +1,38 @@
 import setuptools
-import setup_info
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(**setup_info.setup_info)
+setup_info = {
+    "name": "ro.py",
+    "version": "2.0.0",
+    "author": "jmkdev",
+    "author_email": "jmk@jmksite.dev",
+    "description": "ro.py is a modern object-oriented asynchronous Python wrapper for the Roblox Web API.",
+    "long_description": long_description,
+    "long_description_content_type": "text/markdown",
+    "url": "https://github.com/rbx-libdev/ro.py",
+    "packages": setuptools.find_packages(),
+    "classifiers": [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Framework :: AsyncIO",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
+        "Topic :: Software Development :: Libraries"
+    ],
+    "project_urls": {
+        "Discord": "https://discord.gg/tjRfCbDMSk",
+        "Issue Tracker": "https://github.com/rbx-libdev/ro.py/issues",
+        "GitHub": "https://github.com/rbx-libdev/ro.py/",
+        "Examples": "https://github.com/rbx-libdev/ro.py/tree/main/examples",
+        "Twitter": "https://twitter.com/jmkdev"
+    },
+    "python_requires": '>=3.6',
+    "install_requires": [
+        "httpx"
+    ]
+}
+
+
+setuptools.setup(**setup_info)
