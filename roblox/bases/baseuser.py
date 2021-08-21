7c1054cc8c05e9246cde04d014dcad0eeac007e9
jmkd3v <jmk@jmksite.dev>
2021-08-21 18:43:50 -0400

    Added username history

--- roblox/bases/baseuser.py
+++ roblox/bases/baseuser.py
@@ -1,4 +1,5 @@
 from ..utilities.shared import ClientSharedObject
+from ..utilities.iterators import PageIterator, SortOrder
 
 
 class BaseUser:
@@ -12,3 +13,11 @@ class BaseUser:
         )
         status_data = status_response.json()
         return status_data["status"]
+
+    def username_history(self, limit: int = 10, sort_order: SortOrder = SortOrder.Ascending) -> PageIterator:
+        return PageIterator(
+            shared=self._shared,
+            url=self._shared.url_generator.get_url("users", f"v1/users/{self.id}/username-history"),
+            limit=limit,
+            item_handler=lambda data: data["name"]
+        )
