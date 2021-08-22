cf46205d1e6ccc548f59692ff7cb4c2c14672fd1
jmkd3v <jmk@jmksite.dev>
2021-08-21 21:31:09 -0400

    Added universe favorites

--- roblox/bases/baseuniverse.py
+++ roblox/bases/baseuniverse.py
@@ -5,3 +5,23 @@ class BaseUniverse:
     def __init__(self, shared: ClientSharedObject, universe_id: int):
         self._shared: ClientSharedObject = shared
         self.id: int = universe_id
+
+    async def get_favorite_count(self) -> int:
+        """
+        Returns the universe's favorite count.
+        """
+        favorite_count_response = await self._shared.requests.get(
+            url=self._shared.url_generator.get_url("games", f"v1/games/{self.id}/favorites/count")
+        )
+        favorite_count_data = favorite_count_response.json()
+        return favorite_count_data["favoritesCount"]
+
+    async def is_favorited(self) -> bool:
+        """
+        Returns true if the authenticated user has favorited the universe.
+        """
+        is_favorited_response = await self._shared.requests.get(
+            url=self._shared.url_generator.get_url("games", f"v1/games/{self.id}/favorites")
+        )
+        is_favorited_data = is_favorited_response.json()
+        return is_favorited_data["isFavorited"]
