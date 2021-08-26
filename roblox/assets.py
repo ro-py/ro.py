d0e2cc9cbe5d493d16c61346e2797bd823741f5e
jmkd3v <jmk@jmksite.dev>
2021-08-26 00:17:41 -0300

    added asset types

--- roblox/assets.py
+++ roblox/assets.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from typing import Union, Optional
 from datetime import datetime
 from dateutil.parser import parse
@@ -8,6 +9,87 @@ from .partials.partialuser import PartialUser
 from .partials.partialgroup import AssetPartialGroup
 
 from .bases.baseasset import BaseAsset
+
+asset_type_names = {
+    1: "Image",
+    2: "T-Shirt",
+    3: "Audio",
+    4: "Mesh",
+    5: "Lua",
+    6: "HTML",
+    7: "Text",
+    8: "Hat",
+    9: "Place",
+    10: "Model",
+    11: "Shirt",
+    12: "Pants",
+    13: "Decal",
+    16: "Avatar",
+    17: "Head",
+    18: "Face",
+    19: "Gear",
+    21: "Badge",
+    22: "Group Emblem",
+    24: "Animation",
+    25: "Arms",
+    26: "Legs",
+    27: "Torso",
+    28: "Right Arm",
+    29: "Left Arm",
+    30: "Left Leg",
+    31: "Right Leg",
+    32: "Package",
+    33: "YouTubeVideo",
+    34: "Pass",
+    35: "App",
+    37: "Code",
+    38: "Plugin",
+    39: "SolidModel",
+    40: "MeshPart",
+    41: "Hair Accessory",
+    42: "Face Accessory",
+    43: "Neck Accessory",
+    44: "Shoulder Accessory",
+    45: "Front Accessory",
+    46: "Back Accessory",
+    47: "Waist Accessory",
+    48: "Climb Animation",
+    49: "Death Animation",
+    50: "Fall Animation",
+    51: "Idle Animation",
+    52: "Jump Animation",
+    53: "Run Animation",
+    54: "Swim Animation",
+    55: "Walk Animation",
+    56: "Pose Animation",
+    59: "LocalizationTableManifest",
+    60: "LocalizationTableTranslation",
+    61: "Emote Animation",
+    62: "Video",
+    63: "TexturePack",
+    64: "T-Shirt Accessory",
+    65: "Shirt Accessory",
+    66: "Pants Accessory",
+    67: "Jacket Accessory",
+    68: "Sweater Accessory",
+    69: "Shorts Accessory",
+    70: "Left Shoe Accessory",
+    71: "Right Shoe Accessory",
+    72: "Dress Skirt Accessory",
+    73: "Font Family",
+    74: "Font Face"
+}
+
+
+class AssetType:
+    """
+    Represents a Roblox asset type.
+    """
+    def __init__(self, shared: ClientSharedObject, type_id: int):
+        self._shared: ClientSharedObject = shared
+
+        self.id: int = type_id
+        self.name: Optional[str] = asset_type_names.get(type_id)
 
 
 class EconomyAsset(BaseAsset):
@@ -27,7 +109,7 @@ class EconomyAsset(BaseAsset):
         self.product_id: int = data["ProductId"]  # TODO: make this a BaseProduct
         self.name: str = data["Name"]
         self.description: str = data["Description"]
-        self.asset_type_id: int = data["AssetTypeId"]
+        self.type: AssetType = AssetType(shared=self._shared, type_id=data["AssetTypeId"])
 
         self.creator_type: str = data["Creator"]["CreatorType"]
         self.creator: Union[PartialUser, AssetPartialGroup]
