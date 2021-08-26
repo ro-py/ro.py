0424bb60fb20190942bed877d4bbae3642491f1e
Boegie19 <Bramboegheim@gmail.com>
2021-08-26 21:42:21 +0200

    Iterators.py fix

--- roblox/utilities/iterators.py
+++ roblox/utilities/iterators.py
@@ -8,6 +8,7 @@ class SortOrder(Enum):
     """
     Order in which page data should load in.
     """
+
     Ascending = "Asc"
     Descending = "Desc"
 
@@ -22,21 +23,26 @@ class PageIterator:
     """
 
     def __init__(
-            self,
-            shared: ClientSharedObject,
-            url: str,
-            sort_order: SortOrder = SortOrder.Ascending,
-            limit: int = 10,
-            extra_parameters: dict = None,
-            item_handler: Callable[[dict], any] = None
+        self,
+        shared: ClientSharedObject,
+        url: str,
+        sort_order: SortOrder = SortOrder.Ascending,
+        limit: int = 10,
+        extra_parameters: dict = None,
+        item_handler: Callable = None,
+        handler_kwargs: dict = None
     ):
         self._shared: ClientSharedObject = shared
+
+        if handler_kwargs is None:
+            handler_kwargs = {}
 
         self.url: str = url
         self.sort_order: SortOrder = sort_order.value
         self.limit: int = limit
         self.extra_parameters: dict = extra_parameters
-        self.item_handler: Callable[[dict], any] = item_handler
+        self.item_handler: Callable = item_handler
+        self.handler_kwargs: dict = handler_kwargs
 
         self.previous_page_cursor: str = ""
         self.next_page_cursor: str = ""
@@ -65,22 +71,25 @@ class PageIterator:
         parameters = {
             "cursor": self.next_page_cursor,
             "limit": self.limit,
-            "sortOrder": self.sort_order
+            "sortOrder": self.sort_order,
         }
 
         if self.extra_parameters:
             parameters.update(self.extra_parameters)
 
-        page_response = await self._shared.requests.get(
-            url=self.url,
-            params=parameters
-        )
+        page_response = await self._shared.requests.get(url=self.url, params=parameters)
         page_data = page_response.json()
 
         self.next_page_cursor = page_data["nextPageCursor"]
         self.previous_page_cursor = page_data["previousPageCursor"]
         if self.item_handler:
-            self.data += [self.item_handler(item_data) for item_data in page_data["data"]]
+            self.data += [
+                self.item_handler(
+                    shared=self._shared,
+                    data=item_data,
+                    **self.handler_kwargs
+                ) for item_data in page_data["data"]
+            ]
         else:
             self.data += page_data["data"]
 
