e972a4eac33b870e94a2d48542ecd6fe9f8a78db
Boegie19 <Bramboegheim@gmail.com>
2021-09-05 17:44:10 +0200

    Forgot to add some dots

--- roblox/__init__.py
+++ roblox/__init__.py
@@ -30,10 +30,10 @@ from .client import Client
 from .utilities.exceptions import *
 from .auditlogs import ActionTypes
 from .relationship import RelationshipType
-from thumbnails import ThumbnailState, ThumbnailFormat, ThumbnailReturnPolicy, AvatarThumbnailType
-from universes import UniverseGenre, UniverseAvatarType
-from sharedenums import *
-from bases.basesociallink import SocialLinkType
+from .thumbnails import ThumbnailState, ThumbnailFormat, ThumbnailReturnPolicy, AvatarThumbnailType
+from .universes import UniverseGenre, UniverseAvatarType
+from .sharedenums import *
+from .bases.basesociallink import SocialLinkType
 
 
 class VersionInfo(NamedTuple):
