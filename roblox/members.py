60d8f5efaca95451df8161d12755922a1ea232e9
jmkd3v <jmk@jmksite.dev>
2021-09-28 18:09:45 -0300

    Update members.py

--- roblox/members.py
+++ roblox/members.py
@@ -10,26 +10,25 @@ if TYPE_CHECKING:
 
 
 class Member(PartialUser):
-    def __init__(self, shared: ClientSharedObject, data: dict, group):
+    def __init__(self, shared: ClientSharedObject, data: dict, group: BaseGroup):
         self._shared: ClientSharedObject = shared
 
         super().__init__(shared=self._shared, data=data["user"])
 
         self.role: PartialRole = PartialRole(shared=self._shared, data=data["role"])
         self.group = group
-        self.base_user = self._shared.client.get_base_user(self.id)
 
     async def set_role(self, role_id: int):
-        await self.group.set_role(self.base_user, role_id)
+        await self.group.set_role(self, role_id)
 
     async def set_rank(self, rank_num: int):
-        await self.group.set_rank(self.base_user, rank_num)
+        await self.group.set_rank(self, rank_num)
 
     async def offset_role(self, offset: int):
-        await self.group.offset_role(self.base_user, offset)
+        await self.group.offset_role(self, offset)
 
     async def promote(self):
-        await self.group.promote(self.base_user)
+        await self.group.promote(self)
 
     async def demote(self):
-        await self.group.demote(self.base_user)
+        await self.group.demote(self)
