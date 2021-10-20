5ca61c5d945076dda079d57fb0a233da00b6e7f4
jmkd3v <jmk@jmksite.dev>
2021-10-20 18:09:42 -0300

    Update exceptions.py

--- roblox/utilities/exceptions.py
+++ roblox/utilities/exceptions.py
@@ -4,7 +4,7 @@ Contains exceptions used by ro.py.
 
 """
 
-from typing import Optional
+from typing import Optional, List
 from httpx import Response
 
 
@@ -15,40 +15,16 @@ class RobloxException(Exception):
     pass
 
 
-def _generate_error_string(errors: list):
-    parsed_errors = []
-    for error in errors:
-        # Make each error into a parsed string
-        error_code = error["code"]
-        error_message = error.get("message")
-
-        parsed_error = f"\t{error_code}: {error_message}"
-
-        error_messages = []
-
-        error_user_facing_message = error.get("userFacingMessage")
-        error_field = error.get("field")
-        error_retryable = error.get("retryable")
-
-        if error_user_facing_message:
-            # Add the parenthesis-wrapped user facing message
-            error_messages.append(f"User-facing message: {error_user_facing_message}")
-
-        if error_field:
-            # Add the field, which is the name of the key in the request body that caused the error
-            error_messages.append(f"Field: {error_field}")
-
-        if error_retryable is not None:
-            error_messages.append(f"Retryable: {error_retryable}")
-
-        if error_messages:
-            error_message_string = "\n\t\t".join(error_messages)
-            parsed_error += f"\n\t\t{error_message_string}"
-
-        parsed_errors.append(parsed_error)
-
-    # Turn the parsed errors into a joined string
-    return "\n".join(parsed_errors)
+class ResponseError:
+    """
+    Represents a single error in an "errors" list.
+    """
+    def __init__(self, data: dict):
+        self.code: int = data["code"]
+        self.message: Optional[str] = data.get("message")
+        self.user_facing_message: Optional[str] = data.get("userFacingMessage")
+        self.field: Optional[str] = data.get("field")
+        self.retryable: Optional[str] = data.get("retryable")
 
 
 class HTTPException(RobloxException):
@@ -63,12 +39,35 @@ class HTTPException(RobloxException):
         """
         self.response: Response = response
         self.status: int = response.status_code
+        self.errors: List[ResponseError] = [
+            ResponseError(data=error_data) for error_data in errors
+        ]
         if errors:
-            error_string = _generate_error_string(errors)
+            error_string = self._generate_string()
             super().__init__(f"""{response.status_code} {response.reason_phrase}: {response.url}.\n\nErrors:
 {error_string}""")
         else:
             super().__init__(f"""{response.status_code} {response.reason_phrase}: {response.url}""")
+
+    def _generate_string(self) -> str:
+        parsed_errors = []
+        for error in self.errors:
+            # Make each error into a parsed string
+            parsed_error = f"\t{error.code}: {error.message}"
+            error_messages = []
+
+            error.user_facing_message and error_messages.append(f"User-facing message: {error.user_facing_message}")
+            error.field and error_messages.append(f"Field: {error.field}")
+            error.retryable and error_messages.append(f"Retryable: {error.retryable}")
+
+            if error_messages:
+                error_message_string = "\n\t\t".join(error_messages)
+                parsed_error += f"\n\t\t{error_message_string}"
+
+            parsed_errors.append(parsed_error)
+
+        # Turn the parsed errors into a joined string
+        return "\n".join(parsed_errors)
 
 
 class InvalidRole(RobloxException):
