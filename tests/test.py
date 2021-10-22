8000e9198dd9e73f98e85a9a6c4391f60217750e
jmkd3v <jmk@jmksite.dev>
2021-10-22 00:48:21 -0300

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -26,6 +26,21 @@ async def test_user_id(user_id: int):
     return user.id == alt_user.id == base_user.id
 
 
+async def test_user_id_name(user_id: int):
+    """
+    Tests to see if get_user(user.id) == get_user_by_username(user.name)
+    """
+    user = await client.get_user(user_id)
+    user2 = await client.get_user_by_username(user.name)
+    return user.id == user2.id
+
+
+async def test_user_name_id(name: str):
+    user = await client.get_user_by_username(name)
+    user2 = await client.get_user(user.id)
+    return user.id == user2.id
+
+
 async def main():
     # set up basic authentication
     is_authenticated: bool = False
@@ -44,13 +59,21 @@ async def main():
         logger.warning("Not all checks can complete because ro.py can't authenticate.")
 
     # users
-    assert await test_user_id(1)
-    assert await test_user_id(2)
-    assert await test_user_id(3)
+    assert await test_user_id(1)  # Roblox
+    assert await test_user_id(2)  # John Doe
+    assert await test_user_id(3)  # Jane Doe
 
-    assert await test_user_id(968108160)
-    assert await test_user_id(33655127)
+    assert await test_user_id(968108160)  # jmk (@local_ip)
+    assert await test_user_id(33655127)  # boatbomber
 
+    assert await test_user_id_name(1)
+    assert await test_user_id_name(2)
+    assert await test_user_id_name(3)
+
+    assert await test_user_name_id("Roblox")
+    assert await test_user_name_id("John Doe")
+    assert await test_user_name_id("Jane Doe")
+    
 
 if __name__ == '__main__':
     asyncio.get_event_loop().run_until_complete(main())
