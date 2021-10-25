754de3fbe691b670cd07044e69b99b5523bf9113
jmkd3v <jmk@jmksite.dev>
2021-10-24 23:14:06 -0300

    Update baseitem.py

--- roblox/bases/baseitem.py
+++ roblox/bases/baseitem.py
@@ -14,3 +14,11 @@ class BaseItem:
 
     def __int__(self):
         return self.id
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and other.id == self.id
+
+    def __ne__(self, other):
+        if isinstance(other, self.__class__):
+            return other.id != self.id
+        return True
