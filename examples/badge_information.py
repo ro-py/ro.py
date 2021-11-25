286c3515345fb807858a68b8e1380e966e4f9a36
Auriosi <52893462+Auriosi@users.noreply.github.com>
2021-11-25 16:05:17 -0600

    Create badge_information.py

---
+++ examples/badge_information.py
@@ -0,0 +1,21 @@
+"""
+Grabs badge information.
+"""
+
+import asyncio
+from roblox import Client
+client = Client()
+
+
+async def main():
+    badge = await client.get_badge(66918518)
+
+    print("ID:", badge.id)
+    print("Name:", badge.name)
+    print("Description:", badge.description)
+    print("Enabled:", badge.enabled)
+    print("Awarded Count:", badge.statistics.awarded_count)
+    print("Awarding Universe ID:", badge.awarding_universe.id)
+
+
+asyncio.get_event_loop().run_until_complete(main())
