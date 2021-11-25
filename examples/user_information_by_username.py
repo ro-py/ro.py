d89e59c5b8062968d3e747a166890af85eb5f507
Auriosi <52893462+Auriosi@users.noreply.github.com>
2021-11-25 17:03:17 -0600

    Create user_information_by_username.py

---
+++ examples/user_information_by_username.py
@@ -0,0 +1,22 @@
+"""
+Grabs user information by username.
+"""
+
+import asyncio
+from roblox import Client
+client = Client()
+
+
+async def main():
+    user = await client.get_user_by_username("Roblox", expand = True)
+    status = await user.get_status()
+
+    print("ID:", user.id)
+    print("Name:", user.name)
+    print("Display Name:", user.display_name)
+    print("Created:", user.created.strftime("%m/%d/%Y, %H:%M:%S"))
+    print(f"Status: {status!r}")
+    print(f"Description: {user.description!r}")
+
+
+asyncio.get_event_loop().run_until_complete(main())
