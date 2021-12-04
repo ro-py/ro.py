097ac4cea349336150e460f9be07cfb5fdecf71f
jmkd3v <jmk@jmksite.dev>
2021-12-03 23:39:43 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ setup_info = {
     },
     "python_requires": '>=3.7',
     "install_requires": [
-        "httpx==0.21.*",
-        "python-dateutil~=2.8.2"
+        "httpx>=0.21.0",
+        "python-dateutil>=2.8.0"
     ]
 }
 
